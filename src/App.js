9b5e0e61c03d09eccc24bb1a10f4582f9f6034ba
tianqi <zhangtianqides@yeah.netgit config --global user.email zhangtianqides@yeah.net>
2022-08-17 21:33:18 +0800

    初始化项目搭建

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
 import './App.less';
-
-import { Button } from 'antd';
-
 import { observer } from 'mobx-react-lite'
-import { useStore } from './store/index'
 
 function App() {
-
-  const { counterStore, ListStore } = useStore()
-
   return (
     <div className="App">
-      {counterStore.count}
-      <Button type="primary" onClick={counterStore.addCount}>加一</Button>
-      <br />
-      {ListStore.list.join('-')}
     </div>
   )
 };
