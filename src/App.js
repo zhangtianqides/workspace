09587af2fcae04507ba598e5e592c798602b8659
tianqi <zhangtianqides@yeah.netgit config --global user.email zhangtianqides@yeah.net>
2022-08-18 11:25:30 +0800

    登录页面样式添加

--- src/App.js
+++ src/App.js
@@ -4,13 +4,9 @@ import { observer } from 'mobx-react-lite'
 
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-
 // 因为替换了路径，所以，这里可以直接用@替换，方便后面打包
 import Login from '@/pages/Login'
 import Layout from '@/pages/Layout'
-
-import { Button } from 'antd';
-
 
 
 function App() {
@@ -23,7 +19,6 @@ function App() {
           <Route path='/' element={<Layout />} />
           <Route path='/Login' element={<Login />} />
         </Routes>
-        <Button type="primary">Primary Button</Button>
       </div>
     </BrowserRouter>
   )
--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -1,7 +1,64 @@
 import React from 'react'
+import logo from '@/assets/logo.png'
+import './index.scss'
+
+import { Card } from 'antd'
+import { Button, Checkbox, Form, Input } from 'antd';
+import FormItem from 'antd/es/form/FormItem';
+
+
 
 export default function Login() {
+
     return (
-        <div>Login</div>
+        <div className='login'>
+            <Card className='login-container'>
+                <img className='login-logo' src={logo} alt="" />
+
+                {/* 子项用的事件，需要在form上声明或引用一下 */}
+                <Form
+                    initialValues={{ remember: true, }}
+                    validateTrigger={['onBlur', 'onChange']}>
+                    {/* 登录账号 */}
+                    <Form.Item
+                        name="username"
+                        rules={[{
+                            required: true,
+                            message: '请输入手机号'
+                        },
+                        {
+                            pattern: /^1[3-9]\d{9}$/,
+                            message: '请输入正确的手机号',
+                            validateTrigger: 'onBlur'
+                        }
+                        ]}>
+                        <Input size='large' placeholder='请输入手机号码' />
+                    </Form.Item>
+                    {/* 登录密码 */}
+                    <Form.Item
+                        name="password"
+                        rules={[{
+                            required: true,
+                            message: '请输入密码',
+                        },
+                        {
+                            len: 6,
+                            message: '请输入6位密码',
+                            validateTrigger: 'onBlur'
+                        }
+                        ]}>
+                        <Input.Password size='large' placeholder='请输入密码' />
+                    </Form.Item>
+                    {/* 隐私协议条款 */}
+                    <Form.Item name="remember" valuePropName="checked">
+                        <Checkbox className='login-checkbox-lable'>我已阅读并同意<a href="/">《用户协议》</a>和<a href="/">《隐私条款》</a></Checkbox>
+                    </Form.Item>
+
+                    <FormItem>
+                        <Button type="primary" htmlType="submit" size='langer' block>登录</Button>
+                    </FormItem>
+                </Form>
+            </Card>
+        </div>
     )
 }
